81f28b5f6851972a4830b05fb026a8f474942c31
Winter HoKha (huanhkv) <huanhkv@gmail.com>
2026-04-09 01:14:23 +0700

    feat: add common script

--- setup-fedora.sh
+++ setup-fedora.sh
@@ -1,187 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
-# Set the -e option
-set -e
+source "$(cd "$(dirname "$0")" && pwd)/setup-common.sh"
 
-# Function definition
-backup_path() {
-
-    # Get parameters
-    src="$1"
-    backup_folder="$2"
-
-    if [ -d "$src" ] || [ -f "$src" ]; then
-
-        count=$(find "$backup_folder" -maxdepth 1 -name "$(basename "$src")-*" | wc -l)
-        dest="$backup_folder/$(basename "$src")-$((count))"
-
-        echo "$src exists! Backup to $dest"
-        if [ -L "$src" ]; then
-            echo "$src is a symlink"
-            sudo ln -s "$(readlink "$src")" "$dest"
-	    sudo rm -rf "$src"
-        else
-            echo "$src is NOT a symlink"
-            sudo mv "$src" "$dest"
-        fi
-        echo "Backup completed!"
-    else
-        echo "$src does not exist!"
-    fi
+# ---- OS-specific implementations ----
+install_package() {
+    sudo dnf install -y "$@"
 }
 
-# Backup folder
-backup_folder=$HOME/dotfiles-bak
+install_neovim() {
+    sudo dnf install -y neovim
+}
 
-if [ ! -d "$backup_folder" ]; then
-    echo "$backup_folder does not exist! Create it!"
-    mkdir -p "$backup_folder"
-fi
-
-backup_folder=$(realpath "$backup_folder")
-echo "Backup folder: $backup_folder"
-
-# Config folder
-if [ ! -d "$HOME/.config" ]; then
-    echo "$HOME/.config does not exist! Create it!"
-    mkdir -p "$HOME/.config"
-fi
-
+# ---- Fedora-specific setup ----
 echo "============================== BASE TOOLS =============================="
 
 sudo dnf -y install wget curl git powerline-fonts tree htop tldr ripgrep ncdu gcc gcc-c++ make strace ibus-unikey xclip
 
-# Rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-export PATH=$PATH:$HOME/.cargo/bin
-cargo install eza fd-find
-cargo install --locked bat navi
-
-echo "================================ SHELL ================================="
-
-# Get Alias
-backup_path "$HOME/.config/my-alias.sh" "$backup_folder"
-ln -s "$(realpath dotfiles/.config/my-alias.sh)" "$HOME/.config/my-alias.sh"
-
-# Select shell
-echo "Select Shell:"
-printf "\t1. Zsh\n"
-printf "\t2. Bash\n"
-printf "\t3. Fish\n"
-
-printf "Enter your choice: "
-read -r shell
-
-while [ ! "$shell" -eq 1 ] && [ ! "$shell" -eq 2 ] && [ ! "$shell" -eq 3 ]; do
-    echo "Invalid choice!"
-    printf "Enter your choice again: "
-    read -r shell
-done
-
-# Install shell
-if [ "$shell" -eq 1 ]; then
-    echo "Install ZSH"
-    sudo dnf -y install zsh
-    echo $(zsh --version)
-
-    # Backup ZSH config
-    echo "Backup ZSH config"
-    backup_path "$HOME/.zshrc" "$backup_folder"
-
-    # Install Oh My Zsh
-
-    # Add alias
-    echo "source $HOME/.config/my-alias.sh" >> "$HOME/.zshrc"
-
-elif [ "$shell" -eq 2 ]; then
-    echo "Install Bash"
-
-    # Backup Bash config
-    echo "Backup Bash config"
-    backup_path "$HOME/.bashrc" "$backup_folder"
-
-    # Install Oh My Bash
-    bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
-
-    # Add alias
-    echo "source $HOME/.config/my-alias.sh" >> "$HOME/.bashrc"
-
-else
-    echo "Install Fish shell"
-
-    # Add alias
-fi
-
-echo "================================= TMUX ================================="
-# Install TMUX
-sudo dnf install -y tmux
-
-# Backup TMUX config
-echo "Backup TMUX config"
-backup_path "$HOME/.tmux" "$backup_folder"
-backup_path "$HOME/.tmux.conf" "$backup_folder"
-
-# Install TMUX Plugin Manager
-echo "Install TMUX Plugin Manager (TPM)"
-git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
-
-# Add plugins
-echo "Copy TMUX config from $(realpath dotfiles/.tmux.conf) to $HOME"
-ln -s "$(realpath dotfiles/.tmux.conf)" "$HOME/.tmux.conf"
-
-# Install TMUX plugins
-echo "Install TMUX plugins"
-"$HOME/.tmux/plugins/tpm/bin/install_plugins"
-
-echo "================================= VIM =================================="
-# Install Vim
-echo "Select editor:"
-printf "\t1. Vim\n"
-printf "\t2. Neovim\n"
-
-printf "Enter your choice: "
-read -r editor
-
-while [ ! "$editor" -eq 1 ] && [ ! "$editor" -eq 2 ]; do
-    echo "Invalid choice!"
-    printf "Enter your choice again: "
-    read -r editor
-done
-
-if [ "$editor" -eq 1 ]; then
-
-    echo "Install Vim"
-    sudo dnf install -y vim-enhanced
-
-    # Backup Vim config
-    echo "Backup Vim config"
-    backup_path "$HOME/.vim" "$backup_folder"
-    backup_path "$HOME/.vimrc" "$backup_folder"
-
-    # Install Vim-Plug
-    echo "Install Vim-Plug"
-    sh -c 'curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-    # Add plugins
-    echo "Copy vim config to $HOME"
-    ln -s "dotfiles/.vimrc" "$HOME"
-    ln -s "dotfiles/.vim" "$HOME/.vim"
-    vim +PlugInstall +qall
-
-else
-
-    echo "Install Neovim"
-    sudo dnf install -y neovim
-
-    # Backup NeoVim config
-    echo "Backup NeoVim config"
-    backup_path "$HOME/.config/nvim" "$backup_folder"
-
-    # Add plugins
-    echo "Copy vim config from $(realpath dotfiles/.config/nvim) to $HOME"
-    ln -s "$(realpath dotfiles/.config/nvim)" "$HOME/.config/nvim"
-fi
+setup_rust
+setup_shell
+setup_tmux
+setup_editor
 
 echo "============================= OTHER TOOLs =============================="
 
